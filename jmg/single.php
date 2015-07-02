02e5a3d4995e28f67b9d61b00a25ccea649bdc63
HughBui <hieutrongbui12@gmail.com>
2015-07-02 17:54:58 +0700

    Update single.php

--- jmg/single.php
+++ jmg/single.php
@@ -2,58 +2,36 @@
 
 <div style="height:220px;"></div>
 
-	<div class="container content" style="border:1px solid red;">
-		<div class="row narrow">
-			<div class="col col-span-1 top-left">
-				<h1><?php the_title(); ?></h1>
-			</div>
-		</div>
-	</div>
 
 <?php 
-
 if ( have_posts() ) : while ( have_posts() ) : the_post();
   the_content();
 endwhile;
 
 endif;
-
- 
- 
- 
- // Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
- 
  ?>
  
- 
- <?php query_posts( array( 'posts_per_page' => 3 ) ); ?>
 
+<div class="container content post" style="margin-top: 50px !important;">
+	<div class="row">
+		<div class="col col-span-1 middle-left">
+			<p>Related Posts</p>
+		</div>
+	</div>
+	<div class="row">
+		<?php query_posts( array( 'posts_per_page' => 3 ) ); ?>
+		<?php if(have_posts()): while(have_posts()): the_post(); ?>
+			<div class="col col-span-1 middle-left">
+				<a href="<?php the_permalink(); ?>"><img class="normalImg" src="<?php echo catch_that_image(); ?>"/></a>
+				<p>
+					<?php the_title(); ?><br/>
+					<?php echo get_the_date(); ?>
+				</p>
+			</div>	
+		<?php endwhile; endif; ?>
+	</div>		
+</div>
 
-<?php if(have_posts()): while(have_posts()): the_post(); ?>
+<?php previous_post(); ?>
 
-<h1 class="post-heading"><?php the_title(); ?></h1>
-
-<?php 
-
-echo '<img src="';
- echo catch_that_image();
- echo '" alt="" />';
-
-?>
-
-<div class="content"><?php the_excerpt(); ?></div>
-
-
-<a href="<?php the_permalink(); ?>">Read More</a>
-
-<?php endwhile; endif; ?>
- 
- <?php get_footer(); ?>
+<?php get_footer(); ?>
