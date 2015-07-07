16b51bdd01c35c8b5520bc2c1af671a8c9fae578
HughBui <hieutrongbui12@gmail.com>
2015-07-07 14:34:34 +0700

    chieu 7/7

--- taphouse/content-menuItem.php
+++ taphouse/content-menuItem.php
@@ -2,11 +2,11 @@
 return'
 <div class="row nested wide-fit">
 	<div class="col col-span-9 no-padding">
-		<p>'.$atts["name"].'</p>
-		'.($atts["description"] == "" ? "" : "<p class=\"small\">".$atts["description"]."</p>").'
+		<p>'.$a["name"].'</p>
+		'.($a["description"] == "" ? "" : "<p class=\"small\">".$a["description"]."</p>").'
 	</div>
 	<div class="col col-span-1 no-padding">
-		<p class="price">$'.$atts["price"].'</p>
+		<p class="price">'.($a["publish"] == "false" ? "&nbsp;" : "\$".$a["price"]).'</p>
 	</div>
 </div>
 ';
