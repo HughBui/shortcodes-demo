9bfc20c1a1e5c4ae0417424b23a1bedac953cbe6
HughBui <hieutrongbui12@gmail.com>
2015-07-08 17:35:45 +0700

    chieu 8/7

--- jmg/single.php
+++ jmg/single.php
@@ -14,10 +14,10 @@ background-position: center center; margin-bottom: 100px;">
 <?php 
 if ( have_posts() ) : while ( have_posts() ) : the_post();
  ?>
-<div class="container content post" style="margin: 50px auto 0px auto !important;">
+<div class="container content post">
 	<div class="row">
-		<div class="col col-span-1 middle-left" style="border-top: 1px solid gray; border-bottom: 1px solid gray; line-height: 10px;">
-			<p style="font-size: 12px !important;  line-height: 10px;">
+		<div class="col col-span-1 middle-left info">
+			<p>
 				By<b><?php echo get_the_author(); ?></b> | 
 				<?php echo get_the_date("F jS, Y"); ?> | 
 				<?php echo get_the_category()[0]->cat_name; ?>
@@ -33,29 +33,29 @@ endif;
  ?>
  
 
-<div class="container content post" style="margin-top: 50px !important;">
+<div class="container content post related-posts">
 	<div class="row">
 		<div class="col col-span-1 middle-left">
-			<p>Related Posts</p>
+			<p><b>Related Posts</b></p>
 		</div>
 	</div>
 	<div class="row">
 		<?php query_posts( array( 'posts_per_page' => 3 ) ); ?>
 		<?php if(have_posts()): while(have_posts()): the_post(); ?>
-			<div class="col col-span-1 middle-left" style="padding:10px !important;">
-				<a href="<?php the_permalink(); ?>"><img class="normalImg" src="<?php echo catch_that_image(); ?>"/></a>
+			<div class="col col-span-1 middle-left">
+				<a href="<?php the_permalink(); ?>"><img src="<?php echo catch_that_image(); ?>"/></a>
 				<p>
-					<?php the_title(); ?><br/>
-					<?php echo get_the_date("F jS"); ?>
+					<b><?php the_title(); ?></b><br/>
+					<span><?php echo get_the_date("F jS"); ?></span>
 				</p>
 			</div>	
 		<?php endwhile; endif; ?>
 	</div>		
 </div>
-<div class="container content" style="margin: 50px auto 100px auto !important;">
+<div class="container content post previous">
 	<div class="row">
-		<div class="col col-span-1 middle-left" style="border-top: 1px solid gray; border-bottom: 1px solid gray;">
-			<p style="font-size: 12px !important; line-height: 10px;">< <?php previous_post_link('%link', 'Previous', FALSE); ?></p>
+		<div class="col col-span-1 middle-left info">
+			<p>< <?php previous_post_link('%link', 'Previous', FALSE); ?></p>
 		</div>
 	</div>
 </div>
