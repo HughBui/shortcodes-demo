1d15d5bbd4c15e2fa5cbe69b8ab8e98619859b44
HughBui <hieutrongbui12@gmail.com>
2015-07-09 17:47:35 +0700

    chieu 9/7

---
+++ jmg/content-collapsible.php
@@ -0,0 +1,11 @@
+<?php
+return'
+<div>
+	<span class="toggle" onclick="return toggle(this.parentNode)">-</span>
+	<span class="toggleTitle">'.$a["title"].'</span>
+</div>
+<p class="toggleContent small">
+'.$a["content"].'
+</p>
+';
+?>
