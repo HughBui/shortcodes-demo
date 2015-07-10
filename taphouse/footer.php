9898fa7acbaa7e82f2896d11507197c9bed27d93
HughBui <hieutrongbui12@gmail.com>
2015-07-10 16:21:13 +0700

    chieu 10/7

--- taphouse/footer.php
+++ taphouse/footer.php
@@ -1,6 +1,6 @@
 <div class="container footer">
 	<div class="row">
-		<div class="col col-span-1">
+		<div class="col col-span-2">
 			<h3>CONTACT US</h3>
 			<nobr>T+ <a href="tel:541-382-1454" style="color:white;">541 - 382 - 1454</a></nobr>
 			<nobr>E+ <a href="mailto:whitewatertaphouse@gmail.com" style="color:white;">whitewatertaphouse@gmail.com</a></nobr>
@@ -11,18 +11,26 @@
 			Monday - Thursday: noon - 11pm<br/>
 			Friday - Saturday: noon - 12am<br/>
 			<span style="font-size: 14px; margin-top: 8px; line-height: 20px;">*Hours subject to change according to<br/>city events and private parties</span>
+			<span class="copyright">&copy; White Water Taphouse 2015</span>
+			<div class="gramercy">
+				<span style="color: #606060;">designed by</span>
+				<span>
+				<a href="http://www.gramercystudios.com/" target="_blank">
+				<img class="logo" src="<?php echo get_static_uri('gramercy-logo-grey.png');?>" alt="gramercy logo" style="vertical-align: middle; margin-left: 4px; margin-right: 5px; position: relative; top: 0px" width="22px" height="22px"></a>
+				</span>
+				<a href="http://www.gramercystudios.com/" target="_blank">
+				<img src="<?php echo get_static_uri('gramercy-logotype-grey.png');?>" alt="gramercy logo" style="vertical-align: middle;">
+				</a>
+			</div>
 		</div>
 		<div class="col col-span-1">
 		<h3>FOLLOW US</h3>
 			<div>
 				<img src="<?php echo get_static_uri('facebook_icon_wht.png');?>" style="cursor:pointer; margin:5px;" onclick="location.href='https://www.facebook.com/whitewatertaphouse'"/>
 				<img src="<?php echo get_static_uri('twitter_icon_wht.png');?>" style="cursor:pointer; margin:5px;" onclick="location.href='https://twitter.com/wwtaphousebend'"/>
+				<img src="<?php echo get_static_uri('instagram_icon_wht.png');?>" style="cursor:pointer; margin:5px;" onclick="location.href='http://instagram.com/whitewatertaphouse?ref=badge'"/>
 			</div>
 		</div>
-
-	</div>
-	<div class="copyright">
-		&copy; White Water Taphouse 2015
 	</div>
 </div>
 <?php wp_footer();?> 
