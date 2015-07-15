e7c0fa268740e7fa48e0f348ce3dcc6eb9151924
HughBui <hieutrongbui12@gmail.com>
2015-07-15 17:54:47 +0700

    chieu 15/7

--- jdlc/menu.php
+++ jdlc/menu.php
@@ -3,10 +3,13 @@
 <div class="logo" style="background-image: url(<?php echo get_static_uri('jdlc-logo-top.png'); ?>);">
 </div>
 </a>
-<div class="menu-items">
-  <span><a href="<?php echo get_permalink(get_page_by_path('facilities')); ?>">FACILITIES</a></span>
+<div id="burger-part" class="menu-items">
+	<img id="burger" src="<?php echo get_static_uri('hamburger_icon.png');?>" onclick="toggleMenu()" />
+</div>
+<div id="menu-part" class="menu-items">
+  <span class="selected"><a href="<?php echo get_permalink(get_page_by_path('facilities')); ?>">FACILITIES</a></span>
   <span><a href="<?php echo get_permalink(get_page_by_path('instruments')); ?>">INSTRUMENTS</a></span>
   <span><a href="<?php echo get_permalink(get_page_by_path('research')); ?>">RESEARCH</a></span>
-  <a class="contact-anchor" href="<?php echo get_permalink(get_page_by_path('contact')); ?>"><span class="contact-button">CONTACT</span></a>
+  <span class="contact-button"><a href="<?php echo get_permalink(get_page_by_path('contact')); ?>">CONTACT</a></span>
 </div>
 </div>
