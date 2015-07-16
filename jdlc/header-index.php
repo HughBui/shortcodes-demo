7027326646dccb234a1670aa069d6cbbf8c5c8df
HughBui <hieutrongbui12@gmail.com>
2015-07-16 17:50:51 +0700

    chieu 16/7

--- jdlc/header-index.php
+++ jdlc/header-index.php
@@ -7,12 +7,12 @@
 <?php 
   get_template_part('balloon-map');
   $lbID = jdlc_generate_html_id();
-  $btn = '[lightbox_launcher id="'.$lbID.'" class="play_button"]<img src="'.get_static_uri('play-button.png').'" /><span>INSIDE JDL CENTRE</span>[/lightbox_launcher]';
+  $btn = '[lightbox_launcher id="'.$lbID.'" class="play_button"]<img src="'.get_static_uri('play-button.png').'" /><span class="play_button_text">INSIDE JDL CENTRE</span>[/lightbox_launcher]';
   $btn .= '[lightbox id="'.$lbID.'"]'.lightbox_content_helper_youtube("http://www.youtube.com/embed/NS1JMYco8XA").'[/lightbox]';
   echo parse_shortcode_content($btn);
 ?>
 </div>
-<div class="heading_text">
+<div class="heading_text" style="border: 1px solid red;">
   John de Laeter Centre is a collaborative research venture involving Curtin University,
   The University of Western Australia, the CSIRO and the geological survey of WA.
 </div>
