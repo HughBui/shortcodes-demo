1da8cdca97b714913dabe6894d62da7539513cde
HughBui <hieutrongbui12@gmail.com>
2015-07-17 17:50:35 +0700

    chieu 17/7

--- jdlc/head.php
+++ jdlc/head.php
@@ -1,6 +1,7 @@
 <html <?php language_attributes(); ?>>
   <head>
-  <title><?php wp_title(''); ?></title>
+	<link rel="icon" type="image/png" href="<?php echo get_static_uri('favicon.png');?>" />
+	<title><?php wp_title(''); ?></title>
     <meta charset="<?php bloginfo('charset'); ?>" type="text/css">    
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1">
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
