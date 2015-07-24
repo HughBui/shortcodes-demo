2b36d9c3f0c5863e5b07d8d484bb03f676adc0d3
HughBui <hieutrongbui12@gmail.com>
2015-07-24 17:59:48 +0700

    chieu 24/7

--- jmg/head.php
+++ jmg/head.php
@@ -7,5 +7,6 @@
   <title><?php wp_title(''); ?></title>
   <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css">
   <script src="<?php echo get_template_directory_uri() ?>/script.js" type="text/javascript"></script>
+  <link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
   <?php wp_head(); ?>
 </head>
